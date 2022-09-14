ef736ec5e957d101a8351dfe8ed4a6f3797905d0
jonathon lee <jonnyleealas@gmail.com>
2022-09-14 04:58:55 -0500

    Update app.ts

--- mt/app.ts
+++ mt/app.ts
@@ -1,8 +1,7 @@
 let button_counter: HTMLElement = document.getElementById('counter') as HTMLElement;
 var counter = document.getElementById('number') as HTMLInputElement;
-var count: number = 0;
+var count = 0;
 button_counter.onclick = function () {
     count +=1;
-    console.log("why all the types")
     counter.innerText = `${count}`;
 }
