c2e5c7605286c1bc49fca27a299c7b38bb0e718c
jonathon lee <jonnyleealas@gmail.com>
2022-09-14 16:40:48 -0500

    added arrows to drop down

--- mt/app.ts
+++ mt/app.ts
@@ -1,5 +1,5 @@
 let button_counter: HTMLElement = document.getElementById('counter') as HTMLElement;
-var counter = document.getElementById('number') as HTMLInputElement;
+var counter = document.getElementById('number') as HTMLElement;
 var count = 0;
 button_counter.onclick = function () {
     count +=1;
