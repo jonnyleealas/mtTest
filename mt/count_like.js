da40400562376c9106dc5a6ed80a344d8a8d5922
jonathon lee <jonnyleealas@gmail.com>
2022-09-14 18:26:13 -0500

    removed js file that wasnt being used

--- mt/count_like.js
+++
@@ -1,8 +0,0 @@
-// var button = document.getElementById('counter');
-// var counter = document.getElementById('number');
-// var count = 0;
-// button.onclick = function () {
-//     count +=1;
-//     counter.innerHTML = count;
-// }
-
